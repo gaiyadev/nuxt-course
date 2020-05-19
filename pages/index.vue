a0c55ada0719a517fe2289a908431f8d61b8e195
Gaiya M Obed <gaiyaobed94@gmail.com>
2020-05-19 14:17:51 +0100

    Bug fix to Layout

--- pages/index.vue
+++ pages/index.vue
@@ -8,8 +8,7 @@
 export default {
   data() {
     return {}
-  },
-  layout: 'users'
+  }
 }
 </script>
 
--- pages/users.vue
+++ pages/users.vue
@@ -24,6 +24,7 @@ export default {
     onLoadUser() {
       this.$router.push('/users/' + this.userId)
     }
-  }
+  },
+  layout: 'users'
 }
 </script>
